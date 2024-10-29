89a3dab1816ad92d6e5b79d8aadf15566000ec83
Paolo Giua <paolo.giuaa@gmail.com>
2024-10-29 16:11:52 +0100

    Code refactoring

--- src/blake2b.c
+++ src/blake2b.c
@@ -6,33 +6,32 @@
 
 #define BUF_LENGTH 256
 
-void
-print_hex(const uint8_t* s)
+void print_hex(const uint8_t *s)
 {
-  printf("hash  :");
-  while (*s)
-    printf("%02x", *s++);
-  printf("\n");
+	printf("hash  :");
+	while (*s)
+		printf("%02x", *s++);
+	printf("\n");
 }
 
-int
-main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
 
-  uint8_t buf[] = "The quick brown fox jumps over the lazy dog";
-  uint8_t key[] = "";
-  uint8_t hash[BLAKE2B_OUTBYTES];
-  size_t buflen = strlen((char *)buf);
-  size_t keylen = strlen((char *)key);
-  size_t i;
+	uint8_t buf[] = "The quick brown fox jumps over the lazy dog";
+	uint8_t key[] = "";
+	uint8_t hash[BLAKE2B_OUTBYTES];
+	size_t buflen = strlen((char *)buf);
+	size_t keylen = strlen((char *)key);
+	size_t i;
 
-  for (i = 0; i <= buflen; ++i) {
-    blake2b(hash, BLAKE2B_OUTBYTES, buf, i, key, keylen);
-  }
+	for (i = 0; i <= buflen; ++i)
+	{
+		blake2b(hash, BLAKE2B_OUTBYTES, buf, i, key, keylen);
+	}
 
-  printf("input :%s\n", buf);
-  printf("key   :%s\n", key);
-  print_hex(hash);
+	printf("input :%s\n", buf);
+	printf("key   :%s\n", key);
+	print_hex(hash);
 
-  return 0;
+	return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include "blake2b.h"
 
 /**
  * @brief      Helper function to perform rotation in a 64 bit int
@@ -8,10 +9,9 @@
  *
  * @return     The rotated result
  */
-uint64_t
-rotr64(const uint64_t w, const unsigned c)
+uint64_t rotr64(const uint64_t w, const unsigned c)
 {
-  return (w >> c) | (w << (64 - c));
+	return (w >> c) | (w << (64 - c));
 }
 
 /**
@@ -21,19 +21,18 @@ rotr64(const uint64_t w, const unsigned c)
  *
  * @return     { description_of_the_return_value }
  */
-uint64_t
-load64(const void* src)
+uint64_t load64(const void *src)
 {
 #if defined(NATIVE_LITTLE_ENDIAN)
-  uint64_t w;
-  memcpy(&w, src, sizeof w);
-  return w;
+	uint64_t w;
+	memcpy(&w, src, sizeof w);
+	return w;
 #else
-  const uint8_t* p = (const uint8_t*)src;
-  return ((uint64_t)(p[0]) << 0) | ((uint64_t)(p[1]) << 8) |
-         ((uint64_t)(p[2]) << 16) | ((uint64_t)(p[3]) << 24) |
-         ((uint64_t)(p[4]) << 32) | ((uint64_t)(p[5]) << 40) |
-         ((uint64_t)(p[6]) << 48) | ((uint64_t)(p[7]) << 56);
+	const uint8_t *p = (const uint8_t *)src;
+	return ((uint64_t)(p[0]) << 0) | ((uint64_t)(p[1]) << 8) |
+				 ((uint64_t)(p[2]) << 16) | ((uint64_t)(p[3]) << 24) |
+				 ((uint64_t)(p[4]) << 32) | ((uint64_t)(p[5]) << 40) |
+				 ((uint64_t)(p[6]) << 48) | ((uint64_t)(p[7]) << 56);
 #endif
 }
 
@@ -43,35 +42,33 @@ load64(const void* src)
  * @param      dst   The destination
  * @param[in]  w     word to be stored
  */
-void
-store64(void* dst, uint64_t w)
+void store64(void *dst, uint64_t w)
 {
 #if defined(NATIVE_LITTLE_ENDIAN)
-  memcpy(dst, &w, sizeof w);
+	memcpy(dst, &w, sizeof w);
 #else
-  uint8_t* p = (uint8_t*)dst;
-  p[0] = (uint8_t)(w >> 0);
-  p[1] = (uint8_t)(w >> 8);
-  p[2] = (uint8_t)(w >> 16);
-  p[3] = (uint8_t)(w >> 24);
-  p[4] = (uint8_t)(w >> 32);
-  p[5] = (uint8_t)(w >> 40);
-  p[6] = (uint8_t)(w >> 48);
-  p[7] = (uint8_t)(w >> 56);
+	uint8_t *p = (uint8_t *)dst;
+	p[0] = (uint8_t)(w >> 0);
+	p[1] = (uint8_t)(w >> 8);
+	p[2] = (uint8_t)(w >> 16);
+	p[3] = (uint8_t)(w >> 24);
+	p[4] = (uint8_t)(w >> 32);
+	p[5] = (uint8_t)(w >> 40);
+	p[6] = (uint8_t)(w >> 48);
+	p[7] = (uint8_t)(w >> 56);
 #endif
 }
 
-void
-store32(void* dst, uint32_t w)
+void store32(void *dst, uint32_t w)
 {
 #if defined(NATIVE_LITTLE_ENDIAN)
-  memcpy(dst, &w, sizeof w);
+	memcpy(dst, &w, sizeof w);
 #else
-  uint8_t* p = (uint8_t*)dst;
-  p[0] = (uint8_t)(w >> 0);
-  p[1] = (uint8_t)(w >> 8);
-  p[2] = (uint8_t)(w >> 16);
-  p[3] = (uint8_t)(w >> 24);
+	uint8_t *p = (uint8_t *)dst;
+	p[0] = (uint8_t)(w >> 0);
+	p[1] = (uint8_t)(w >> 8);
+	p[2] = (uint8_t)(w >> 16);
+	p[3] = (uint8_t)(w >> 24);
 #endif
 }
 
@@ -81,9 +78,8 @@ store32(void* dst, uint32_t w)
  * @param      S     blake2b_state instance
  * @param[in]  inc   The increment
  */
-void
-blake2b_increment_counter(blake2b_state* S, const uint64_t inc)
+void blake2b_increment_counter(blake2b_state *S, const uint64_t inc)
 {
-  S->t[0] += inc;
-  S->t[1] += (S->t[0] < inc);
+	S->t[0] += inc;
+	S->t[1] += (S->t[0] < inc);
 }
